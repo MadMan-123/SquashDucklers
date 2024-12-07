f257ba03238c43b67acc4872b23bf6c79104913e
ocean <132059573+ocean621@users.noreply.github.com>
2024-12-07 11:55:01 +0000

    temporally storing changes

--- Assets/Scripts/AI/EnemyMove.cs
+++ Assets/Scripts/AI/EnemyMove.cs
@@ -1,81 +1,147 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem.Controls;
 
 
 public class EnemyMove : MonoBehaviour
 {
+    public NavMeshAgent agent;
     public Transform player;
-    public float move_sp;
-    public Animator ani;
-    public float playerRange;
-    public LayerMask IsPlayer;
+    public LayerMask whatIsGround, whatIsPlayer;
 
-    //Attacking
+    //attack
+    private Animator m_animator;
     public float timeBetweenAttacks;
     bool alreadyAttacked;
 
-    public bool distancetoplayer;
-   
-    private void Start()
+    [SerializeField] private float slapForce = 2.5f;
+    [SerializeField] private float slapRadius = 0.75f;
+    [SerializeField] private float howMuchUp = 0.75f;
+    [SerializeField] private float slapDamage = 5;
+    [SerializeField] PhysicMaterial slapMat;
+
+    //States
+    public float sightRange, attackRange;
+    public bool InSightRange, InAttackRange;
+    public bool spawned;
+
+
+
+
+    // Start is called before the first frame update
+    void Awake()
     {
-        distancetoplayer = Physics.CheckSphere(transform.position, playerRange, IsPlayer);
-        if (distancetoplayer) AttackPlayer();
-
-
-        
-        ani = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        spawned = true;
     }
 
-    private void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        player = GameObject.FindWithTag("Player").transform ; 
-        
-        
-        if (distancetoplayer) AttackPlayer();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        InSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        InAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-
-        Vector3 newtarget = player.position;
-        newtarget.y = transform.position.y;
-       // newtarget.x = transform.position.x;
-        transform.LookAt(newtarget);
-
-
-        //transform.LookAt(player);
-    transform.position = Vector3.MoveTowards(transform.position, player.position,move_sp * Time.deltaTime);
+        if (InSightRange && !InAttackRange) Chase();
+        if (InSightRange && InAttackRange) Attack();
+        if (spawned) Jump();
 
     }
 
 
-
-    private void AttackPlayer()
+    private void Jump()
     {
 
-        
+        spawned = false;
+    }
+
+    private void Chase()
+    {
+       
+
+        agent.SetDestination(player.position);
+    }
+
+    private void Attack()
+    {
+        //stop movement while slapping
+        agent.SetDestination(transform.position);
+
+
 
         if (!alreadyAttacked)
         {
             ///Attack code here
+            Collider[] colliders = new Collider[10];
+            int count = Physics.OverlapSphereNonAlloc(transform.position + transform.forward, slapRadius, colliders);
+            bool canSlapSfx = false;
+            //for each collider in colliders, if can get rigidbody, add force
+            for (int i = 0; i < count; i++)
+            {
+                float extraForce = 0f;
+                if (colliders[i].gameObject == gameObject || !colliders[i].TryGetComponent(out Rigidbody rb)) continue; //if same object continue
+                canSlapSfx = true;
+                if (colliders[i].TryGetComponent(out Health health))         //if has component health then
+                {
+                    health.TakeDamage(gameObject, slapDamage);
+                    extraForce = (health.GetHealth() / health.GetMaxHealth()) * slapForce;
+                }
+                if (colliders[i].TryGetComponent(out PlayerControler pc))
+                {
+                    StartCoroutine(ReduceFriction(colliders[i].gameObject, pc, extraForce / 5));
+                    pc.Ragdoll();
+                }
+                if (colliders[i].TryGetComponent(out AIBrain brain))
+                {
+                    //disable the agent and enable kinematic
+                    StartCoroutine(brain.ReenableAgent(brain.knockDownTime));
+                    brain.ChangeState(AIBrain.State.Chase);
 
-            ani.Play("slap");
+                }
+                rb.AddForce(((transform.forward) * (slapForce + extraForce / 5)) + ((transform.up * howMuchUp) * slapForce / 5), ForceMode.Impulse);
 
+            }
+            if (canSlapSfx)
+                SoundManager.PlayAudioClip("Slap", transform.position + transform.forward, 1f);
 
+            ///End of attack code
 
             alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+
+            if (alreadyAttacked)
+            {
+
+                Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            }
+
         }
     }
     private void ResetAttack()
     {
+
         alreadyAttacked = false;
     }
 
     private void OnDrawGizmosSelected()
     {
-     
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, playerRange);
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 
+    IEnumerator ReduceFriction(GameObject player, PlayerControler pc, float slapForce)
+    {
+        pc.deceleration = 0.001f;
+        PhysicMaterial temp = null;
+        gameObject.GetComponent<CapsuleCollider>().material = slapMat;
+        yield return new WaitForSeconds(1 + slapForce);
+        pc.deceleration = 0.1f;
+        gameObject.GetComponent<CapsuleCollider>().material = temp;
+    }
+
+
 }
+
--- Assets/Scripts/AI/ShipCrewAI.cs
+++ Assets/Scripts/AI/ShipCrewAI.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SocialPlatforms.Impl;
@@ -5,51 +6,53 @@ using UnityEngine.SocialPlatforms.Impl;
 public class ShipCrewAI : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public LayerMask whatIsGround;
     public bool walking;
-    //move around ship
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
-
+    public bool gocannon;
+    public Transform posA;
+    public Transform posB;
 
 
     private void Awake()
     {
         walking = true;
+  CanonBall();
         agent = GetComponent<NavMeshAgent>();
     }
 
-    private void Update()
-    {
-        if (walking) RunAround();
-
-    }
-
-    private void RunAround()
+        private void OnTriggerEnter(Collider collision)
     {
 
-        if (!walkPointSet) SearchWalkPoint();
-
-        if (walkPointSet)
-            agent.SetDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        //Walkpoint reached
-        if (distanceToWalkPoint.magnitude < 1f)
-            walkPointSet = false;
+        if (collision.gameObject.CompareTag("canonball"))
+        {
+            Canon();
+            gocannon = true;
+            Debug.Log("cannonballed");
+        }
+        else if (collision.gameObject.CompareTag("canon"))
+        {
+            gocannon = false;
+            CanonBall();
+        }
     }
-    private void SearchWalkPoint()
+
+
+    private void CanonBall()
     {
-        //Calculate random point in range
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-            walkPointSet = true;
+        if (!gocannon) 
+        { 
+        agent.SetDestination(posA.position);
     }
 
- }
+    
+    }
+
+    private void Canon()
+    {
+        if (gocannon)
+        {
+            agent.SetDestination(posB.position);
+        }
+       
+    }
+
+}
